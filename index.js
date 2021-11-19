fbaa4ecc25a5843d56a4678e62f5d954e45803e7
shadowbattalion <hafizspaceumar@gmail.com>
2021-11-19 11:25:26 +0000

    project setup

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const hbs = require("hbs");
+const wax = require("wax-on");
+require("dotenv").config();
+
+// create an instance of express app
+let app = express();
+
+// set the view engine
+app.set("view engine", "hbs");
+
+// static folder
+app.use(express.static("public"));
+
+// setup wax-on
+wax.on(hbs.handlebars);
+wax.setLayoutPath("./views/layouts");
+
+// enable forms
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+
+//routes
+const home_routes = require('./routes/home');
+
+
+
+async function main() {
+
+    app.use('/', home_routes)
+
+
+ 
+}
+
+main();
+
+app.listen(3000, () => {
+  console.log("Server has started");
+});
+
