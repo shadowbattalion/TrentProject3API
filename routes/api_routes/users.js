e3871d69bc492c40520cdf5a8a69f8d837633655
shadowbattalion <hafizspaceumar@gmail.com>
2021-12-04 17:11:05 +0000

    test

--- routes/api_routes/users.js
+++ routes/api_routes/users.js
@@ -114,8 +114,7 @@ router.post('/user-logout', [refresh_check_api], async (req,res)=>{
 
 
 router.get('/user-profile', [auth_check_api], (req, res) => {
-    console.log("=================================================")
-    console.log(req.user)
+
     let user = req.user
 
     res.json({
